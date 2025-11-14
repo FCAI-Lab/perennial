2f55a0f5929531397abcb5b982b888df205d8d52
Tej Chajed <chajed@wisc.edu>
2025-11-13 20:32:02 -0600

    Organize channel examples

--- testdata/examples/channel/muxer.go
+++ testdata/examples/channel/muxer.go
@@ -74,35 +74,6 @@ func Muxer(c chan streamold) {
 	}
 }
 
-func CancellableMapServer(s streamold, done chan struct{}) {
-	for {
-		select {
-		case in, ok := <-s.req:
-			if !ok {
-				return
-			}
-			s.res <- s.f(in)
-		case <-done:
-			return
-		}
-	}
-}
-
-// 4. CancellableMuxer - muxer with cancellation
-func CancellableMuxer(c chan streamold, done chan struct{}, errMsg *string) string {
-	for {
-		select {
-		case s, ok := <-c:
-			if !ok {
-				return "serviced all requests"
-			}
-			go CancellableMapServer(s, done)
-		case <-done:
-			return *errMsg
-		}
-	}
-}
-
 func makeGreeting() string {
 	// Start muxer
 	mux := make(chan streamold, 2)
