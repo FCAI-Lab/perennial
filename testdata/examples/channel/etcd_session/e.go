ee94da8e689386d65ad1b897d4463d3073141b49
Upamanyu Sharma <upamanyu@mit.edu>
2026-03-10 22:35:08 -0400

    Start sketching etcd-inspired example

---
+++ testdata/examples/channel/etcd_session/e.go
@@ -0,0 +1,60 @@
+package etcd_session
+
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+var sessionc chan struct{} = make(chan struct{})
+var mu sync.Mutex
+
+// Mock something that might take a while, and can fail
+func newSession() error {
+	// ...
+	return nil
+}
+
+// Mock something that might take a while.
+func waitForSessionExpiration() {
+}
+
+func monitorSession() {
+	for {
+		waitForSessionExpiration()
+
+		mu.Lock()
+		select {
+		case <-sessionc:
+			sessionc = make(chan struct{})
+		default:
+		}
+		mu.Unlock()
+
+		err := newSession()
+		if err != nil {
+			continue
+		}
+
+		mu.Lock()
+		close(sessionc)
+		mu.Unlock()
+	}
+}
+
+func waitSession[A any](cancel <-chan A) error {
+	mu.Lock()
+	s := sessionc
+	mu.Unlock()
+	select {
+	case <-s:
+		return nil
+	case <-cancel:
+		return errors.New("cancelled")
+	}
+}
+
+func sessionMain() {
+	go monitorSession()
+	waitSession(time.After(time.Second))
+}
