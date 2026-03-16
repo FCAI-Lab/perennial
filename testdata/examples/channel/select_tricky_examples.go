436361e4e56850cdf3d7a67f2a24dc08ee67e811
Tej Chajed <chajed@wisc.edu>
2026-03-16 09:27:18 -0500

    Clean up select examples

--- testdata/examples/channel/select_tricky_examples.go
+++ testdata/examples/channel/select_tricky_examples.go
@@ -1,6 +1,6 @@
 package channel_examples
 
-// Show that it isn't possible to have 2 nonblocking ops that match.
+// Example with 2 nonblocking ops that should not match.
 func select_nb_not_ready() {
 	ch := make(chan struct{})
 	go func() {
@@ -22,15 +22,16 @@ func select_nb_guaranteed_ready() {
 	x := make(chan int)
 	close(x)
 	select {
+	// non-blocking receive is guaranteed to execute
 	case <-x:
 	default:
+		// must prove unreachable
 		close(x)
 	}
 }
 
-// Show that a nonblocking select does not take a send case
-// when the buffered channel is already full.
-func select_nb_full_buffer_no_panic() {
+// Non-blocking send cannot send on a full buffer
+func select_nb_full_buffer_not_ready() {
 	// 1. Buffered channel of size 1
 	ch := make(chan int, 1)
 
@@ -45,36 +46,5 @@ func select_nb_full_buffer_no_panic() {
 		panic("unreachable")
 	default:
 		// benign: correct behavior
-	}
-}
-
-// Unverifiable: Panic is irreducible
-func select_nb_buffer_space_panic() {
-	// Create a buffered channel with capacity 1
-	ch := make(chan int, 1)
-
-	// Nonblocking select:
-	// the send case is enabled and may be chosen.
-	select {
-	case ch <- 0:
-		// This panic is reachable
-		panic("bad")
-	default:
-	}
-}
-
-// Blocking select: vacuously verifiable due to deadlock
-func select_nb_buffer_space_deadlock() {
-	// Create a buffered channel with capacity 1
-	ch := make(chan int, 1)
-
-	// First send fills the buffer
-	ch <- 0
-
-	// Blocks indefinitely on send.
-	select {
-	case ch <- 0:
-		// This code is unreachable
-		panic("unreachable")
 	}
 }
