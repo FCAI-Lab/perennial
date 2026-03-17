3a8df2a923f82ff2bdedc85560aaf3d07fcdc5a6
Tej Chajed <chajed@wisc.edu>
2026-03-16 20:26:25 -0500

    Make Actris example a bit shorter

--- testdata/examples/channel/actris_example.go
+++ testdata/examples/channel/actris_example.go
@@ -2,8 +2,7 @@ package channel_examples
 
 // prog3 from Actris 2.0 intro: https://arxiv.org/pdf/2010.15030
 func DSPExample() int {
-	c := make(chan any)
-	signal := make(chan any)
+	c, signal := make(chan any), make(chan any)
 
 	go func() {
 		ptr := (<-c).(*int)
@@ -11,8 +10,7 @@ func DSPExample() int {
 		signal <- struct{}{}
 	}()
 
-	val := 40
-	ptr := &val // create reference to 40
+	ptr := new(40)
 	c <- ptr
 	<-signal
 	return *ptr // dereference to get 42
